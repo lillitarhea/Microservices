00537fff698e70b4f4698b9756d3fcdf9ae72341
lillitarhea <lillitarhea@gmail.com>
2023-01-16 23:09:06 +0530

    first commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into currency_exchange(id, conversion_multiple, environment, currency_from, currency_to)
+values (1001, 80, '', 'USD', 'INR' );
+insert into currency_exchange(id, conversion_multiple, environment, currency_from, currency_to)
+values (1002, 90, '', 'EUR', 'INR' );
+insert into currency_exchange(id, conversion_multiple, environment, currency_from, currency_to)
+values (1003, 25, '', 'AUD', 'INR' );
